b8e8856a86149d46f0e94900b0362ca1d970c960
cem eröksüz <cemeroksuz@outlook.com>
2021-01-17 16:23:06 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " " + musteri.Soyadi + " Adlı Müşteri Sisteme Eklendi\n");
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " " + musteri.Soyadi + " Adlı Müşteri Sistemden Çıkartıldı\n");
+
+
+        }
+
+        public void MusteriListele(Musteri[] musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("Müşteri Id: " + musteri.Id + "\nMüşteri Adı: " + musteri.Adi + "\nMüşteri Soyadı: " + musteri.Soyadi + "\nMüşteri Yaşı: " + musteri.Yas + "\nMüşteri Mail: " + musteri.email);
+                Console.WriteLine("--------------------------------------------");
+                   
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Adi = "Cem";
+            musteri1.Soyadi = "Eröksüz";
+            musteri1.Yas = 27;
+            musteri1.email = "cemeroksuz@gmail.com";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Adi = "Engin";
+            musteri2.Soyadi = "Demiroğ";
+            musteri2.Yas = 34;
+            musteri2.email = "engindemirog@gmail.com";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Adi = "Can";
+            musteri3.Soyadi = "Ercan";
+            musteri3.Yas = 30;
+            musteri3.email = "canercan@gmail.com";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManager musteriMananger = new MusteriManager();
+
+            musteriMananger.MusteriEkle(musteri1);
+            musteriMananger.MusteriEkle(musteri2);
+            musteriMananger.MusteriEkle(musteri3);
+
+            musteriMananger.MusteriListele(musteriler);
+
+            musteriMananger.MusteriSil(musteri3);
+
+            
+
+            
+           
+
+
+        }
+
+
+    }
+
+    
+
+}
